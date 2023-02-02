533280e2fb0a1f5f57a1b720a69e2374ff1aedf3
2190031 <2190031@ipisa.edu.do>
2023-02-02 10:00:39 -0400

    links del indice 2

--- fill-index.php
+++ fill-index.php
@@ -10,12 +10,12 @@ require('conn.php');
         echo "
         <input id='$row[id]' type='radio' value='".$row['content']."' name='helpDoc' 
         onchange='showTitleAndCont(`".$row['title']."`, this.value)' hidden>
-        <a href='#content' class='link-primary' id='scroll-nav-link' onclick='
+        <a href='#content' class='link-primary lh-lg' id='scroll-nav-link' onclick='
             document.getElementById(".$row['id'].").checked = true;
             var event = new Event(`change`);
             document.getElementById(`$row[id]`).dispatchEvent(event);
-        ' style='line-height: 40px; margin-bottom: 10px;'>".$row['title']."</a>
-        <button type='button' class='btn btn-primary btn-sm' id='scroll-nav-btn' style='float: right; margin: 0px;' onclick='editCont(`".$row['id']."`, `".$row['title']."`, `". $row['content']."`)'>Editar</button>
+        '>".$row['title']."</a>
+        <button type='button' class='btn btn-primary btn-sm float-end' id='scroll-nav-btn' onclick='editCont(`".$row['id']."`, `".$row['title']."`, `". $row['content']."`)'>Editar</button>
         <br>";
         }
     } else {

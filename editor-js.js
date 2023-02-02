a87dfe3950a8dc493ea5946a85b4a79dda0cebce
2190031 <2190031@ipisa.edu.do>
2023-02-02 15:07:08 -0400

    funcion de foto y video

--- editor-js.js
+++ editor-js.js
@@ -1,18 +1,50 @@
+var toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],
+  [{'header': [1, 2, 3, 4, 5, 6, false]}],
+  [{'font': []}],
+  [{'size': ['small', false, 'large', 'huge']}],
+  [{'color': []},{'background':[]}],
+  [{'list': 'ordered'},{'list': 'bullet'}],
+  [{'indent': '-1'}, {'indent': '+1'}],
+  [{'align': []}],
+  ['link', 'image', 'video']
+];
+
+
 var quill = new Quill('#editor', {
-    // placeholder: 'lorem ipsum dolor sit amet',
-    theme: 'snow'
-  });
+  modules: {
+    toolbar: toolbarOptions
+  },
+  theme: 'snow'
+});
+
+var range = quill.getSelection();
+if (range) {
+  var imageUrl = 'https://img2.rtve.es/i/?w=1600&i=1634194794500.jpg';
+  quill.insertEmbed(range.index, 'image', imageUrl, Quill.sources.USER);
+  quill.setSelection(range.index + 1, Quill.sources.SILENT);
+}
+
   var title = new Quill('#title-editor', {
     // placeholder: 'Digite el título de la sección',
     theme: 'bubble'
   });
+
   title.focus();
+
   function jsSave(){
       let titulo = title.container.firstChild.innerText;
     //   console.log(titulo);
       let contenido = quill.container.firstChild.innerHTML;
     //   console.log(contenido);
-      fetch('insert.php?titulo=' + titulo + '&contenido=' + contenido);
+      fetch('insert.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+      body: `titulo=${titulo}&contenido=${contenido}`
+      });
+
       console.log('Insertado');
       var myVar = setInterval(myFunc, 1000);
       function myFunc() {

b365a856505207cff758bf87fbfaa741d14812e0
2190031 <2190031@ipisa.edu.do>
2023-02-11 09:27:43 -0400

    archivo gitignore

--- conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "c55h32o5n4Mg", "document");
-
-if ($conn->connect_error) {
-    die("Error: ".$conn->connect_errno ." " .$conn->connect_error);
-}
-?>

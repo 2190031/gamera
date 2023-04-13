4a829d6cf51c7325a4f0a6d41926144e80b3c0f5
2190031 <2190031@ipisa.edu.do>
2023-04-13 12:46:40 -0400

    funcion de indice

--- edit_nav.php
+++ edit_nav.php
@@ -1,6 +1,11 @@
 <?php 
     $cont = $_POST['cont'];
+    $newScript = $_POST['newScript'];
     echo $cont;
 
     file_put_contents('0.1indice_copy.html', $cont);
+    file_put_contents('indice-js.php', $newScript);
+    file_put_contents('indice-js.js', $newScript);
+
+    die();
 ?>
--- editor.php
+++ editor.php
@@ -2,7 +2,7 @@
 include("conn.php");
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="UTF-8">
@@ -33,26 +33,13 @@ include("conn.php");
   <!-- Core build with no theme, formatting, non-essential modules -->
   <!-- <link href="//cdn.quilljs.com/1.3.6/quill.core.css" rel="stylesheet">
   <script src="//cdn.quilljs.com/1.3.6/quill.core.js"></script> -->
-  <link href="quill\quill.core.css" rel="stylesheet"> 
+  <link href="quill\quill.core.css" rel="stylesheet">
   <script src="quill\quill.core.js"></script>
 
   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script> -->
   <link rel="stylesheet" href="bootstrap\css\bootstrap.min.css">
   <script src="bootstrap\js\bootstrap.js"></script>
-
-  <script>
-    function getLiElements() {
-      const content = document.getElementById('indice').innerHTML;
-      const lines = content.split('\n');
-      const liElements = lines.filter(line => line.includes('<li><a href="#" id'));
-      document.getElementById('indice').innerHTML = liElements.toString();
-      console.log(liElements);
-      liElements.splice(0, 0, '<li><a href="#" class>Nuevo Link</a></li>');
-      console.log(liElements)
-      document.getElementById('indice2').innerHTML = liElements.toString();
-    }
-  </script>
 </head>
 
 <body onload="cargarIndice()" class="">
@@ -74,9 +61,10 @@ include("conn.php");
 
     <div id="h" class="d-none">
       <h2 class="h2">Seccion a la que pertenece</h2>
-      <select class="form-select" id="parent">
-        <?php include('fill-select.php'); ?>
-      </select>
+      <input class="form-control" list="parent" id="parents" placeholder="Type to search...">
+      <datalist id="parent">
+        <?php include('fill-datalist.php'); ?>
+      </datalist>
       <br><br>
     </div>
 
@@ -122,7 +110,6 @@ include("conn.php");
   </div>
 
   <hr>
-  <button class="btn btn-outline-success" onclick="removeFromIndex(document.getElementById('s-49'))">Borrar</button>
 
   <h1 class="h1">Agregar al índice</h1>
   <div class='indice'>
@@ -133,8 +120,8 @@ include("conn.php");
 
 
   <!-- Include the Quill library -->
-  <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <!-- <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
   <script src="quill/quill.js"></script>
   <script src="jquery/jquery-3.6.4.min.js"></script>
 
@@ -152,7 +139,9 @@ include("conn.php");
       });
     }
   </script>
-  </script>
+  <div class="d-none" id="indice-js">
+    
+  </div>
 </body>
 
 </html>

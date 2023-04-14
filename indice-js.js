95e936c2e4126374c291400e499577474ec76798
2190031 <2190031@ipisa.edu.do>
2023-04-14 17:52:54 -0400

    funcion de eliminar del indice

--- indice-js.js
+++ indice-js.js
@@ -3,4 +3,15 @@ $(document).ready(function () {
 $('#p-164').click(function() { $("#contenido").load("1/jnrgkjlsgkjnsbk.html"); }); 
 $('#s-77').click(function() { $("#contenido").load("2/jnrgkjlsgkjnsbk.html"); }); 
 $('#c-30').click(function() { $("#contenido").load("4/jnrgkjlsgkjnsbk.html"); }); 
+$('#p-165').click(function() { $("#contenido").load("1/zxcvbhjkhgfdfghjkjhgfdhgfdjhgfkjhgflkjhgfd.html"); }); 
+$('#s-78').click(function() { $("#contenido").load("2/fghjklkjhg.html"); }); 
+$('#s-80').click(function() { $("#contenido").load("2/fghjklkjhg.html"); }); 
+$('#t-26').click(function() { $("#contenido").load("3/fghjk.html"); }); 
+$('#c-31').click(function() { $("#contenido").load("4/fghjk.html"); }); 
+$('#p-166').click(function() { $("#contenido").load("1/fghjklkjhgf.html"); }); 
+$('#s-81').click(function() { $("#contenido").load("2/fghjklkjhgf.html"); }); 
+$('#t-27').click(function() { $("#contenido").load("3/fghjklkjhgf.html"); }); 
+$('#c-32').click(function() { $("#contenido").load("4/fghjklkjhgf.html"); }); 
+$('#s-82').click(function() { $("#contenido").load("2/gdnfmg,hfndbsvdfmg,hndb.html"); }); 
+$('#c-33').click(function() { $("#contenido").load("4/kijhgfds.html"); }); 
 });

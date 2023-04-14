f65ebac36309eb0ad2b9f59673df5cbf36de9d2c
2190031 <2190031@ipisa.edu.do>
2023-04-14 15:44:58 -0400

    prueba

--- indice-js.js
+++ indice-js.js
@@ -1,3 +1,6 @@
 $(document).ready(function () { 
 
+$('#p-164').click(function() { $("#contenido").load("1/jnrgkjlsgkjnsbk.html"); }); 
+$('#s-77').click(function() { $("#contenido").load("2/jnrgkjlsgkjnsbk.html"); }); 
+$('#c-30').click(function() { $("#contenido").load("4/jnrgkjlsgkjnsbk.html"); }); 
 });

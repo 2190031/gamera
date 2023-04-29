85c2fa3bff73ac3f9162cdf9839698a7acf24fc0
2190031 <2190031@ipisa.edu.do>
2023-04-29 11:01:56 -0400

    movida de archivos js a carpeta js

---
+++ js/indice-js.js
@@ -0,0 +1,30 @@
+$(document).ready(function () { 
+
+$('#p-164').click(function() { $("#contenido").load("1/jnrgkjlsgkjnsbk.html"); }); 
+$('#s-77').click(function() { $("#contenido").load("2/jnrgkjlsgkjnsbk.html"); }); 
+$('#c-30').click(function() { $("#contenido").load("4/jnrgkjlsgkjnsbk.html"); }); 
+$('#p-165').click(function() { $("#contenido").load("1/zxcvbhjkhgfdfghjkjhgfdhgfdjhgfkjhgflkjhgfd.html"); }); 
+$('#s-78').click(function() { $("#contenido").load("2/fghjklkjhg.html"); }); 
+$('#s-80').click(function() { $("#contenido").load("2/fghjklkjhg.html"); }); 
+$('#t-26').click(function() { $("#contenido").load("3/fghjk.html"); }); 
+$('#c-31').click(function() { $("#contenido").load("4/fghjk.html"); }); 
+$('#p-166').click(function() { $("#contenido").load("1/fghjklkjhgf.html"); }); 
+$('#s-81').click(function() { $("#contenido").load("2/fghjklkjhgf.html"); }); 
+$('#t-27').click(function() { $("#contenido").load("3/fghjklkjhgf.html"); }); 
+$('#c-32').click(function() { $("#contenido").load("4/fghjklkjhgf.html"); }); 
+$('#s-82').click(function() { $("#contenido").load("2/gdnfmg,hfndbsvdfmg,hndb.html"); }); 
+$('#c-33').click(function() { $("#contenido").load("4/kijhgfds.html"); }); 
+$('#s-83').click(function() { $("#contenido").load("2/fghjkldfghjkfghjfghjk.html"); }); 
+$('#s-86').click(function() { $("#contenido").load("2/fghvsdfbsfbsfhdsdss.html"); }); 
+$('#p-169').click(function() { $("#contenido").load("1/khgvouyv.html"); }); 
+$('#p-170').click(function() { $("#contenido").load("1/vbfsbfdsbsb.html"); }); 
+$('#p-171').click(function() { $("#contenido").load("1/fsvsdvdsvsdv.html"); }); 
+$('#p-172').click(function() { $("#contenido").load("1/egñugsgr.html"); }); 
+$('#p-173').click(function() { $("#contenido").load("1/mcgh_ghmcmgfmmmcgh.html"); }); 
+$('#p-174').click(function() { $("#contenido").load("1/,ghvvghjjhv.html"); }); 
+$('#p-175').click(function() { $("#contenido").load("1/ulyvfuylvyvlyiviyuviyuvivuy.html"); }); 
+$('#p-176').click(function() { $("#contenido").load("1/bbbdfdfbdbfdfbdfb.html"); }); 
+$('#p-177').click(function() { $("#contenido").load("1/erbretbsbtstsst.html"); }); 
+$('#p-178').click(function() { $("#contenido").load("1/mhgcvhj,jvh,vhj,vj,hvjh,hvjhvjvhhvjvhj,vhjvhhv,vhvh,vhjjh,vvhj,.html"); }); 
+$('#p-179').click(function() { $("#contenido").load("1/1234567890-wertyuiop[sdfghjkl;asdfghjkl;dsfghjkldfgdfcvgygjhv,vhjvkhhjvcm_hvhvmvhgjcvhmk.html"); }); 
+});

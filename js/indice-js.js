e133361324a0805de1c7f3e08e8c6e30430863ef
Annibel14.Annibel <Annibel14.2190007@ipisa.edu.do>
2023-05-11 09:57:07 -0400

    Desplazar indice

--- js/indice-js.js
+++ js/indice-js.js
@@ -1,4 +1,7 @@
 $(document).ready(function () { 
 
 $('#p-18').click(function() { $("#contenido").load("1/cristoteama.html"); }); 
+$('#s-7').click(function() { $("#contenido").load("2/BLIIIIIIII.html"); }); 
+$('#t-10').click(function() { $("#contenido").load("3/blooooooooooooooo.html"); }); 
+$('#s-8').click(function() { $("#contenido").load("2/blaaaaaaaaaaa.html"); }); 
 });

d2ab537d2d40c7c35256834a4fbf33ddc835e025
Annibel14.Annibel <Annibel14.2190007@ipisa.edu.do>
2023-05-13 09:21:09 -0400

    Indice cambios 1

--- js/indice-js.js
+++ js/indice-js.js
@@ -1,3 +1,9 @@
 $(document).ready(function () { 
 
+$('#s-1').click(function() { $("#contenido").load("2/Prueba2-PN-Prueba1.html"); }); 
+$('#t-1').click(function() { $("#contenido").load("3/3raPrueba-PN-Prueba2.html"); }); 
+$('#p-2').click(function() { $("#contenido").load("1/OtraPrueba.html"); }); 
+$('#s-3').click(function() { $("#contenido").load("2/soss-PN-Prueba1
+Prueba2
+3raPrueba.html"); }); 
 });

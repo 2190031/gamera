5654609d4f8fa7ee14d019d6e3b232b573f56070
2190031 <2190031@ipisa.edu.do>
2023-05-20 09:36:55 -0400

    Limpiado de archivos

--- js/indice-js.js
+++ js/indice-js.js
@@ -1,6 +1,3 @@
 $(document).ready(function () { 
 
-$('#p-1').click(function() { $("#contenido").load("1/hjhdvfdcdv-PN-hjhdvfdcdv.html"); }); 
-$('#p-5').click(function() { $("#contenido").load("1/kvj.hbsljkhvf.html"); }); 
-$('#s-1').click(function() { $("#contenido").load("2/hjhdvfdcdv-PN-hjhdvfdcdv.html"); }); 
 });

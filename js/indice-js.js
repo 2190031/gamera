c4416943414664cdb3596c1d933ba27e4c8ad99d
rdgzkei <2190005@ipisa.edu.do>
2023-06-10 09:10:04 -0400

    Se actualizo el indice elementos y el indice-js

--- js/indice-js.js
+++ js/indice-js.js
@@ -1,12 +1,4 @@
 
 $(document).ready(function () {
-$('#s-4').click(function () { $("#contenido").load("2/Prueba_2-PN-PortadaPrueba.html"); });
 
-$('#p-2').click(function () { $("#contenido").load("1/qwertyuiopjgvsf.html"); });
-
-$('#p-3').click(function () { $("#contenido").load("1/iygiyg.html"); });
-
-$('#p-4').click(function () { $("#contenido").load("1/pionpip.html"); });
-
-$('#s-7').click(function () { $("#contenido").load("2/lkugljbk;-PN-pionpip.html"); });
 });
